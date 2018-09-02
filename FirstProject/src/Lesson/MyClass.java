5ac0adef55d5096823defb3731c652ac25d5931d
User <User@User-THINK>
2018-09-02 11:50:51 +0100

    FirstCommit

---
+++ FirstProject/src/Lesson/MyClass.java
@@ -0,0 +1,14 @@
+package Lesson;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Praise The Lord Pranali loves Johnson God created man and woman");
+        
+        System.out.println("Johny should change his behavivor");
+        System.out.println("Elsa loves Amy");
+        System.out.print("Amy also loves Elsa");
+	}
+
+}
